67a6bbf1e64cec375c17e2cceaf45f0b0d310a21
Michael McCulloch <michael.james.mcculloch@gmail.com>
2018-03-14 18:17:20 -0600

    ?

--- RayTracing/Tracer.cpp
+++ RayTracing/Tracer.cpp
@@ -6,18 +6,17 @@ int main(int argc, char *argv[]){
 }
 
 //Generate normalized Direction vectors
-vector<vector<vec3>> generateRays(int d, float fov) {
+vector<vec3> generateRays(int d, float fov) {
 
 	float z = d / (2 * tan(fov / 2.0));
 	vec3 topLeft = vec3((float)d / -2.0 + 0.5f, (float)d/2.0 - 0.5f, z);
 
-	vector<vector<vec3>> image = {};
+
+	vector<vec3> dirs = {};
 	for (int i = 0; i < d; i++) {
-		vector<vec3> row = {};
 		for (int j = 0; j < d; j++) {
-			row.push_back(normalize(topLeft + vec3(i, -j, 0)));
+			dirs.push_back(normalize(topLeft + vec3(i, -j, 0)));
 		}
-		image.push_back(row);
 	}
-	return image;
+	return dirs;
 }
